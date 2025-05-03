62fe7a9d6c90c92b94bffcc3c7cf005d4ffcadd5
mofrim <mofrim@posteo.de>
2025-05-03 14:04:03 +0200

    feat: add rgb values to colors.h as comments

--- src/include/colors.h
+++ src/include/colors.h
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 22:31:00 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/03/21 10:07:34 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/05/03 14:18:21 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,150 +16,150 @@
 #ifndef COLORS_H
 # define COLORS_H
 
-# define ALICE_BLUE           "f0f8ff"
-# define ANTIQUE_WHITE        "faebd7"
-# define AQUA                 "00ffff"
-# define AQUAMARINE           "7fffd4"
-# define AZURE                "f0ffff"
-# define BEIGE                "f5f5dc"
-# define BISQUE               "ffe4c4"
-# define BLACK                "000000"
-# define BLANCHED_ALMOND      "ffebcd"
-# define BLUE                 "0000ff"
-# define BLUE_VIOLET          "8a2be2"
-# define BROWN                "a52a2a"
-# define BURLYWOOD            "deb887"
-# define CADET_BLUE           "5f9ea0"
-# define CHARTREUSE           "7fff00"
-# define CHOCOLATE            "d2691e"
-# define CORAL                "ff7f50"
-# define CORNFLOWER_BLUE      "6495ed"
-# define CORNSILK             "fff8dc"
-# define CRIMSON              "dc143c"
-# define CYAN                 "00ffff"
-# define DARK_BLUE            "00008b"
-# define DARK_CYAN            "008b8b"
-# define DARK_GOLDENROD       "b8860b"
-# define DARK_GRAY            "a9a9a9"
-# define DARK_GREEN           "006400"
-# define DARK_KHAKI           "bdb76b"
-# define DARK_MAGENTA         "8b008b"
-# define DARK_OLIVE_GREEN     "556b2f"
-# define DARK_ORANGE          "ff8c00"
-# define DARK_ORCHID          "9932cc"
-# define DARK_RED             "8b0000"
-# define DARK_SALMON          "e9967a"
-# define DARK_SEA_GREEN       "8fbc8f"
-# define DARK_SLATE_BLUE      "483d8b"
-# define DARK_SLATE_GRAY      "2f4f4f"
-# define DARK_TURQUOISE       "00ced1"
-# define DARK_VIOLET          "9400d3"
-# define DEEP_PINK            "ff1493"
-# define DEEP_SKY_BLUE        "00bfff"
-# define DIM_GRAY             "696969"
-# define DODGER_BLUE          "1e90ff"
-# define FIREBRICK            "b22222"
-# define FLORAL_WHITE         "fffaf0"
-# define FOREST_GREEN         "228b22"
-# define FUCHSIA              "ff00ff"
-# define GAINSBORO            "dcdcdc"
-# define GHOST_WHITE          "f8f8ff"
-# define GOLD                 "ffd700"
-# define GOLDENROD            "daa520"
-# define GRAY                 "bebebe"
-# define WEB_GRAY             "808080"
-# define GREEN                "00ff00"
-# define WEB_GREEN            "008000"
-# define GREEN_YELLOW         "adff2f"
-# define HONEYDEW             "f0fff0"
-# define HOT_PINK             "ff69b4"
-# define INDIAN_RED           "cd5c5c"
-# define INDIGO               "4b0082"
-# define IVORY                "fffff0"
-# define KHAKI                "f0e68c"
-# define LAVENDER             "e6e6fa"
-# define LAVENDER_BLUSH       "fff0f5"
-# define LAWN_GREEN           "7cfc00"
-# define LEMON_CHIFFON        "fffacd"
-# define LIGHT_BLUE           "add8e6"
-# define LIGHT_CORAL          "f08080"
-# define LIGHT_CYAN           "e0ffff"
-# define LIGHT_GOLDENROD      "fafad2"
-# define LIGHT_GRAY           "d3d3d3"
-# define LIGHT_GREEN          "90ee90"
-# define LIGHT_PINK           "ffb6c1"
-# define LIGHT_SALMON         "ffa07a"
-# define LIGHT_SEA_GREEN      "20b2aa"
-# define LIGHT_SKY_BLUE       "87cefa"
-# define LIGHT_SLATE_GRAY     "778899"
-# define LIGHT_STEEL_BLUE     "b0c4de"
-# define LIGHT_YELLOW         "ffffe0"
-# define LIME                 "00ff00"
-# define LIME_GREEN           "32cd32"
-# define LINEN                "faf0e6"
-# define MAGENTA              "ff00ff"
-# define MAROON               "b03060"
-# define WEB_MAROON           "800000"
-# define MEDIUM_AQUAMARINE    "66cdaa"
-# define MEDIUM_BLUE          "0000cd"
-# define MEDIUM_ORCHID        "ba55d3"
-# define MEDIUM_PURPLE        "9370db"
-# define MEDIUM_SEA_GREEN     "3cb371"
-# define MEDIUM_SLATE_BLUE    "7b68ee"
-# define MEDIUM_SPRING_GREEN  "00fa9a"
-# define MEDIUM_TURQUOISE     "48d1cc"
-# define MEDIUM_VIOLET_RED    "c71585"
-# define MIDNIGHT_BLUE        "191970"
-# define MINT_CREAM           "f5fffa"
-# define MISTY_ROSE           "ffe4e1"
-# define MOCCASIN             "ffe4b5"
-# define NAVAJO_WHITE         "ffdead"
-# define NAVY_BLUE            "000080"
-# define OLD_LACE             "fdf5e6"
-# define OLIVE                "808000"
-# define OLIVE_DRAB           "6b8e23"
-# define ORANGE               "ffa500"
-# define ORANGE_RED           "ff4500"
-# define ORCHID               "da70d6"
-# define PALE_GOLDENROD       "eee8aa"
-# define PALE_GREEN           "98fb98"
-# define PALE_TURQUOISE       "afeeee"
-# define PALE_VIOLET_RED      "db7093"
-# define PAPAYA_WHIP          "ffefd5"
-# define PEACH_PUFF           "ffdab9"
-# define PERU                 "cd853f"
-# define PINK                 "ffc0cb"
-# define PLUM                 "dda0dd"
-# define POWDER_BLUE          "b0e0e6"
-# define PURPLE               "a020f0"
-# define WEB_PURPLE           "800080"
-# define REBECCA_PURPLE       "663399"
-# define RED                  "ff0000"
-# define ROSY_BROWN           "bc8f8f"
-# define ROYAL_BLUE           "4169e1"
-# define SADDLE_BROWN         "8b4513"
-# define SALMON               "fa8072"
-# define SANDY_BROWN          "f4a460"
-# define SEA_GREEN            "2e8b57"
-# define SEASHELL             "fff5ee"
-# define SIENNA               "a0522d"
-# define SILVER               "c0c0c0"
-# define SKY_BLUE             "87ceeb"
-# define SLATE_BLUE           "6a5acd"
-# define SLATE_GRAY           "708090"
-# define SNOW                 "fffafa"
-# define SPRING_GREEN         "00ff7f"
-# define STEEL_BLUE           "4682b4"
-# define TAN                  "d2b48c"
-# define TEAL                 "008080"
-# define THISTLE              "d8bfd8"
-# define TOMATO               "ff6347"
-# define TURQUOISE            "40e0d0"
-# define VIOLET               "ee82ee"
-# define WHEAT                "f5deb3"
-# define WHITE                "ffffff"
-# define WHITE_SMOKE          "f5f5f5"
-# define YELLOW               "ffff00"
-# define YELLOW_GREEN         "9acd32"
+# define ALICE_BLUE           "#f0f8ff" // 240,248,255
+# define ANTIQUE_WHITE        "#faebd7" // 250,235,215
+# define AQUA                 "#00ffff" // 0,255,255
+# define AQUAMARINE           "#7fffd4" // 127,255,212
+# define AZURE                "#f0ffff" // 240,255,255
+# define BEIGE                "#f5f5dc" // 245,245,220
+# define BISQUE               "#ffe4c4" // 255,228,196
+# define BLACK                "#000000" // 0,0,0
+# define BLANCHED_ALMOND      "#ffebcd" // 255,235,205
+# define BLUE                 "#0000ff" // 0,0,255
+# define BLUE_VIOLET          "#8a2be2" // 138,43,226
+# define BROWN                "#a52a2a" // 165,42,42
+# define BURLYWOOD            "#deb887" // 222,184,135
+# define CADET_BLUE           "#5f9ea0" // 95,158,160
+# define CHARTREUSE           "#7fff00" // 127,255,0
+# define CHOCOLATE            "#d2691e" // 210,105,30
+# define CORAL                "#ff7f50" // 255,127,80
+# define CORNFLOWER_BLUE      "#6495ed" // 100,149,237
+# define CORNSILK             "#fff8dc" // 255,248,220
+# define CRIMSON              "#dc143c" // 220,20,60
+# define CYAN                 "#00ffff" // 0,255,255
+# define DARK_BLUE            "#00008b" // 0,0,139
+# define DARK_CYAN            "#008b8b" // 0,139,139
+# define DARK_GOLDENROD       "#b8860b" // 184,134,11
+# define DARK_GRAY            "#a9a9a9" // 169,169,169
+# define DARK_GREEN           "#006400" // 0,100,0
+# define DARK_KHAKI           "#bdb76b" // 189,183,107
+# define DARK_MAGENTA         "#8b008b" // 139,0,139
+# define DARK_OLIVE_GREEN     "#556b2f" // 85,107,47
+# define DARK_ORANGE          "#ff8c00" // 255,140,0
+# define DARK_ORCHID          "#9932cc" // 153,50,204
+# define DARK_RED             "#8b0000" // 139,0,0
+# define DARK_SALMON          "#e9967a" // 233,150,122
+# define DARK_SEA_GREEN       "#8fbc8f" // 143,188,143
+# define DARK_SLATE_BLUE      "#483d8b" // 72,61,139
+# define DARK_SLATE_GRAY      "#2f4f4f" // 47,79,79
+# define DARK_TURQUOISE       "#00ced1" // 0,206,209
+# define DARK_VIOLET          "#9400d3" // 148,0,211
+# define DEEP_PINK            "#ff1493" // 255,20,147
+# define DEEP_SKY_BLUE        "#00bfff" // 0,191,255
+# define DIM_GRAY             "#696969" // 105,105,105
+# define DODGER_BLUE          "#1e90ff" // 30,144,255
+# define FIREBRICK            "#b22222" // 178,34,34
+# define FLORAL_WHITE         "#fffaf0" // 255,250,240
+# define FOREST_GREEN         "#228b22" // 34,139,34
+# define FUCHSIA              "#ff00ff" // 255,0,255
+# define GAINSBORO            "#dcdcdc" // 220,220,220
+# define GHOST_WHITE          "#f8f8ff" // 248,248,255
+# define GOLD                 "#ffd700" // 255,215,0
+# define GOLDENROD            "#daa520" // 218,165,32
+# define GRAY                 "#bebebe" // 190,190,190
+# define WEB_GRAY             "#808080" // 128,128,128
+# define GREEN                "#00ff00" // 0,255,0
+# define WEB_GREEN            "#008000" // 0,128,0
+# define GREEN_YELLOW         "#adff2f" // 173,255,47
+# define HONEYDEW             "#f0fff0" // 240,255,240
+# define HOT_PINK             "#ff69b4" // 255,105,180
+# define INDIAN_RED           "#cd5c5c" // 205,92,92
+# define INDIGO               "#4b0082" // 75,0,130
+# define IVORY                "#fffff0" // 255,255,240
+# define KHAKI                "#f0e68c" // 240,230,140
+# define LAVENDER             "#e6e6fa" // 230,230,250
+# define LAVENDER_BLUSH       "#fff0f5" // 255,240,245
+# define LAWN_GREEN           "#7cfc00" // 124,252,0
+# define LEMON_CHIFFON        "#fffacd" // 255,250,205
+# define LIGHT_BLUE           "#add8e6" // 173,216,230
+# define LIGHT_CORAL          "#f08080" // 240,128,128
+# define LIGHT_CYAN           "#e0ffff" // 224,255,255
+# define LIGHT_GOLDENROD      "#fafad2" // 250,250,210
+# define LIGHT_GRAY           "#d3d3d3" // 211,211,211
+# define LIGHT_GREEN          "#90ee90" // 144,238,144
+# define LIGHT_PINK           "#ffb6c1" // 255,182,193
+# define LIGHT_SALMON         "#ffa07a" // 255,160,122
+# define LIGHT_SEA_GREEN      "#20b2aa" // 32,178,170
+# define LIGHT_SKY_BLUE       "#87cefa" // 135,206,250
+# define LIGHT_SLATE_GRAY     "#778899" // 119,136,153
+# define LIGHT_STEEL_BLUE     "#b0c4de" // 176,196,222
+# define LIGHT_YELLOW         "#ffffe0" // 255,255,224
+# define LIME                 "#00ff00" // 0,255,0
+# define LIME_GREEN           "#32cd32" // 50,205,50
+# define LINEN                "#faf0e6" // 250,240,230
+# define MAGENTA              "#ff00ff" // 255,0,255
+# define MAROON               "#b03060" // 176,48,96
+# define WEB_MAROON           "#800000" // 128,0,0
+# define MEDIUM_AQUAMARINE    "#66cdaa" // 102,205,170
+# define MEDIUM_BLUE          "#0000cd" // 0,0,205
+# define MEDIUM_ORCHID        "#ba55d3" // 186,85,211
+# define MEDIUM_PURPLE        "#9370db" // 147,112,219
+# define MEDIUM_SEA_GREEN     "#3cb371" // 60,179,113
+# define MEDIUM_SLATE_BLUE    "#7b68ee" // 123,104,238
+# define MEDIUM_SPRING_GREEN  "#00fa9a" // 0,250,154
+# define MEDIUM_TURQUOISE     "#48d1cc" // 72,209,204
+# define MEDIUM_VIOLET_RED    "#c71585" // 199,21,133
+# define MIDNIGHT_BLUE        "#191970" // 25,25,112
+# define MINT_CREAM           "#f5fffa" // 245,255,250
+# define MISTY_ROSE           "#ffe4e1" // 255,228,225
+# define MOCCASIN             "#ffe4b5" // 255,228,181
+# define NAVAJO_WHITE         "#ffdead" // 255,222,173
+# define NAVY_BLUE            "#000080" // 0,0,128
+# define OLD_LACE             "#fdf5e6" // 253,245,230
+# define OLIVE                "#808000" // 128,128,0
+# define OLIVE_DRAB           "#6b8e23" // 107,142,35
+# define ORANGE               "#ffa500" // 255,165,0
+# define ORANGE_RED           "#ff4500" // 255,69,0
+# define ORCHID               "#da70d6" // 218,112,214
+# define PALE_GOLDENROD       "#eee8aa" // 238,232,170
+# define PALE_GREEN           "#98fb98" // 152,251,152
+# define PALE_TURQUOISE       "#afeeee" // 175,238,238
+# define PALE_VIOLET_RED      "#db7093" // 219,112,147
+# define PAPAYA_WHIP          "#ffefd5" // 255,239,213
+# define PEACH_PUFF           "#ffdab9" // 255,218,185
+# define PERU                 "#cd853f" // 205,133,63
+# define PINK                 "#ffc0cb" // 255,192,203
+# define PLUM                 "#dda0dd" // 221,160,221
+# define POWDER_BLUE          "#b0e0e6" // 176,224,230
+# define PURPLE               "#a020f0" // 160,32,240
+# define WEB_PURPLE           "#800080" // 128,0,128
+# define REBECCA_PURPLE       "#663399" // 102,51,153
+# define RED                  "#ff0000" // 255,0,0
+# define ROSY_BROWN           "#bc8f8f" // 188,143,143
+# define ROYAL_BLUE           "#4169e1" // 65,105,225
+# define SADDLE_BROWN         "#8b4513" // 139,69,19
+# define SALMON               "#fa8072" // 250,128,114
+# define SANDY_BROWN          "#f4a460" // 244,164,96
+# define SEA_GREEN            "#2e8b57" // 46,139,87
+# define SEASHELL             "#fff5ee" // 255,245,238
+# define SIENNA               "#a0522d" // 160,82,45
+# define SILVER               "#c0c0c0" // 192,192,192
+# define SKY_BLUE             "#87ceeb" // 135,206,235
+# define SLATE_BLUE           "#6a5acd" // 106,90,205
+# define SLATE_GRAY           "#708090" // 112,128,144
+# define SNOW                 "#fffafa" // 255,250,250
+# define SPRING_GREEN         "#00ff7f" // 0,255,127
+# define STEEL_BLUE           "#4682b4" // 70,130,180
+# define TAN                  "#d2b48c" // 210,180,140
+# define TEAL                 "#008080" // 0,128,128
+# define THISTLE              "#d8bfd8" // 216,191,216
+# define TOMATO               "#ff6347" // 255,99,71
+# define TURQUOISE            "#40e0d0" // 64,224,208
+# define VIOLET               "#ee82ee" // 238,130,238
+# define WHEAT                "#f5deb3" // 245,222,179
+# define WHITE                "#ffffff" // 255,255,255
+# define WHITE_SMOKE          "#f5f5f5" // 245,245,245
+# define YELLOW               "#ffff00" // 255,255,0
+# define YELLOW_GREEN         "#9acd32" // 154,205,50
 
 #endif
