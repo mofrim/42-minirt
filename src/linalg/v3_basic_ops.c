47c4611d9a36d6dad611d4c45fe8a3fe0e81c247
mofrim <mofrim@posteo.de>
2025-05-25 22:54:16 +0200

    cleanup: remove obsolete vec3 functions

--- src/linalg/v3_basic_ops.c
+++ src/linalg/v3_basic_ops.c
@@ -6,11 +6,11 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 13:33:53 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/05/25 19:00:14 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/05/25 22:52:23 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "v3.h"
+#include "minirt.h"
 
 /* Add a scalar to each component of a t_v3. Returns result. */
 t_v3	v3_add_scalar(t_v3 v, double d)
@@ -46,4 +46,10 @@ t_v3	v3_mult(t_v3 v, double d)
 	v.y *= d;
 	v.z *= d;
 	return (v);
+}
+
+/* Print a t_v3 and its name. */
+void	v3_print(t_v3 v, char *name)
+{
+	printf("v3-%s: (%f, %f, %f)\n", name, v.x, v.y, v.z);
 }
