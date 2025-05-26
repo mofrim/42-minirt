a8050208d9fe670e2ec4ce99e79b5c18a821b5d4
mofrim <mofrim@posteo.de>
2025-05-26 20:08:48 +0200

    chore: clean-up parsing.h

--- src/include/parsing.h
+++ src/include/parsing.h
@@ -6,14 +6,13 @@
 /*   By: zrz <zrz@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 18:22:49 by jroseiro          #+#    #+#             */
-/*   Updated: 2025/05/26 10:46:47 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/05/26 20:08:13 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-// to prevent cross includes
 # include "objects.h"
 # include <stdbool.h>
 # include <stddef.h>
@@ -23,7 +22,7 @@
 typedef struct s_scene	t_scene;
 typedef struct s_mrt	t_mrt;
 
-// Define the different types of tokens
+/* The token types. */
 typedef enum e_tokentype
 {
 	TOKEN_TYPE_KEYWORD,
@@ -33,7 +32,7 @@ typedef enum e_tokentype
 	TOKEN_TYPE_NULL
 }	t_tokentype;
 
-// Define the structure of a token
+/* The token struct. */
 typedef struct s_token
 {
 	t_tokentype	type;
@@ -54,17 +53,15 @@ typedef struct s_tokenizer
 	bool	valid;
 }	t_tokenizer;
 
-/********** PARSER FUNCTIONS **********/
+/********** Parser Functions **********/
 
-// Main parsing functions
-int			get_scene_from_parser(t_tokenizer *tokenizer, t_scene *scene);
 t_scene		*parse_scene(char *scene_filename, t_mrt *mrt);
 t_scene		*init_scene(void);
 int			lineparse_scenefile(char *file_content, t_scene *scene);
 void		handle_token_keyword(t_scene *scene, t_tokenizer *tokenizer,
 				char *key);
 
-// Object parsers
+/* Object parsing funcs. */
 t_colr		*parse_ambient_light(t_tokenizer *tokenizer);
 t_camera	*parse_camera(t_tokenizer *tokenizer);
 t_light		*parse_light(t_tokenizer *tokenizer);
@@ -76,7 +73,7 @@ t_triangle	*parse_triangle(t_tokenizer *tokenizer);
 t_circle	*parse_circle(t_tokenizer *tokenizer);
 t_hyper		*parse_hyper(t_tokenizer *tok);
 
-// Parser utilities
+/* Parsing utils. */
 t_v3		parse_v3(t_tokenizer *tok);
 t_v3		parse_v3_from_parts(char **parts);
 float		parse_pos_num(t_tokenizer *tok);
@@ -86,15 +83,17 @@ bool		has_rt_ext(char *filename);
 bool		validate_color(char **parts, bool *valid);
 bool		validate_vector(char **parts, bool *valid);
 char		*get_tokstr(t_tokenizer *tok);
+void		skip_whitespace(t_tokenizer *tokenizer);
+int			is_coordinate(t_tokenizer *tokenizer);
 
-/********** TOKENIZER FUNCTIONS **********/
+/********** Tokenizer Functions **********/
 
 t_tokenizer	*tokenizer_new(char *input);
 t_token		*get_next_token(t_tokenizer *tokenizer);
 void		token_free(t_token **token);
 t_token		*get_null_token(void);
 
-// Token type parsers
+/* Low-level token parsing funcs. */
 t_token		*parse_coordinate(t_tokenizer *tokenizer);
 t_token		*parse_keyword(t_tokenizer *tokenizer);
 t_token		*parse_number_token(t_tokenizer *tokenizer);
@@ -103,12 +102,5 @@ char		*parse_nmap_maybe(t_tokenizer *tok);
 bool		parse_bump_maybe(t_tokenizer *tok);
 bool		parse_checker_maybe(t_tokenizer *tok);
 int			parse_hyperchecker_maybe(t_tokenizer *tok);
-
-// Helper functions for tokenizing
-void		skip_whitespace(t_tokenizer *tokenizer);
-int			is_coordinate(t_tokenizer *tokenizer);
-void		skip_sign(t_tokenizer *tokenizer);
-void		parse_digits(t_tokenizer *tokenizer);
-void		parse_decimal(t_tokenizer *tokenizer);
 
 #endif
